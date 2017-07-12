7c31f3cfb1be907e8e9237db218137efb16c7ca0
Kevin <whiteowl0001@gmail.com>
2017-07-12 05:57:25 +0000

    user name

--- resources/views/admin/carousel.blade.php
+++ resources/views/admin/carousel.blade.php
@@ -1,7 +1,6 @@
 @extends('admin.admin-master')
 @section('content')
- <div id="page-wrapper">
-
+         <div id="page-wrapper">
             <div class="container-fluid">
 
                 <!-- Page Heading -->
@@ -15,13 +14,17 @@
                                 <i class="fa fa-dashboard"></i> Dashboard
                             </li>
                         </ol>
+                        <div>
+                            <form>
+                                <input type="file" name="file[]" multiple><br/>
+                                <input type="submit">
+                            </form>
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
-
             </div>
             <!-- /.container-fluid -->
-
         </div>
         <!-- /#page-wrapper -->
 @stop
