d3adcf4effceb72542ffa7da722e485ed9d59e91
Kevin <whiteowl0001@gmail.com>
2017-07-14 01:00:27 +0000

    controller

--- app/Http/Controllers/CarouselController.php
+++ app/Http/Controllers/CarouselController.php
@@ -15,33 +15,24 @@ class CarouselController extends Controller
         public function index(){
             return view('admin.carousel');
         }
-        public function multiple_upload(){
-            $files = Input::file('images');
-            $file_count = count($files);
-            $uploadcount = 0;
+        
+        public function multiple_upload(Request $request){
             
-            foreach($files as $file){
-                $rules = array('file' => 'required');
-                $validator = Validator::make(array('file' => $file), $rules);
-                if($validator->passes()){
-                    $destinationPath = 'carousel';
-                    $filename = $file->getClientOriginalName();
-                    $upload_success = $file->move($destinationPath, $filename);
-                    $uploadcount ++;
-                    
-                    $extension = $file->getClientOriginalExtention();
-                    $entry = new Carousel();
-                    $entry->mime = $file->getClientMimeType();
-                    $entry->original_filename = $filename;
-                    $entry->filename = $file->getFilename().'.'.$extension;
-                    $entry->save();
-                }
+            if($request->hasFile('images')){
+                $files = $request->file('images');
+                foreach($files as $file){
+                        $filename = $file->getClientOriginalName();
+                        $extension = $file->getClientOriginalExtention();
+                        $destinationPath = base_path() . '/public/carousel/';
+                        $upload_success = $file->move($destinationPath, $filename);
+                        
+                        $entry = new Carousel();
+                        $entry->mime = $file->getClientMimeType();
+                        $entry->original_filename = $filename;
+                        $entry->filename = $file->getFilename().'.'.$extension;
+                        $entry->save();
+                 }
             }
-            if($uploadcount == $file_count){
-                Session::flash('success', 'Upload Successfully!');
-                return redirect()->back();
-            }else{
-                return redirect()->back();
-            }
+           return redirect()->back();
         }
 }
