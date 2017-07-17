8c63f029b3c02056cb061b558a156ba64a9a3a57
Kevin <whiteowl0001@gmail.com>
2017-07-17 05:20:26 +0000

    Created Eloquent Video

--- resources/views/admin/video.blade.php
+++ resources/views/admin/video.blade.php
@@ -30,19 +30,19 @@
                                 </form>
             		        </div>
             		    </div>
-                        <div class="col-md-6">
+                        <div class="col-md-12">
                              <div class="panel panel-default">
                                 <div class="panel-heading">Videos</div>
                                     <div class="panel-body">
                             @foreach ($Videos as $Video)
                                 <div class="container">
-                                    <div class="row">
+                                    
                                         <div class="col-sm-4 video-layout">
                                            <div class="embed-responsive embed-responsive-16by9">
                                                 <iframe class="embed-responsive-item"  src="{{$Video->video_name}}" allowfullscreen></iframe>       
                                             </div>
                                         </div>
-                                    </div>
+                                    
                                 </div>
                             @endforeach 
                         </div>
