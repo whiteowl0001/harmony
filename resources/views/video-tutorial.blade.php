820f780f355311fa0d6b2eea030dcf194b9971ce
Kevin <whiteowl0001@gmail.com>
2017-07-17 05:37:47 +0000

    Created Eloquent Video

--- resources/views/video-tutorial.blade.php
+++ resources/views/video-tutorial.blade.php
@@ -4,16 +4,27 @@
 <div class="video">
     <div class="container">
         <div class="row">
-             @foreach ($Videos as $Video)
+            <div class="col-sm-4 video-layout">
+               <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item"  src="https://www.youtube.com/embed/OSK3mQpPvxg?rel=0" allowfullscreen></iframe>       
+                </div>
+            </div>
+            <div class="col-sm-4 video-layout">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/Gz51ciTi4ZA?rel=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--             @foreach ($Videos as $Video)
                 <div class="col-sm-4 video-layout">
                    <div class="embed-responsive embed-responsive-16by9">
                           <iframe class="embed-responsive-item"  src="https://www.youtube.com/embed/{{$Video->video_name}}" allowfullscreen></iframe>       
                     </div>
                 </div>
             @endforeach 
-        </div>
-    </div>
-</div>
+            -->
  <script src="js/jquery.js"></script>
     <!--<script src="js/bootstrap.min.js"></script>-->
     <script src="https://maps.google.com/maps/api/js?sensor=true"></script>
