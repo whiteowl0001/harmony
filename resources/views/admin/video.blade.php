86ff3c90ea0c868fe9236973120e31e9bd6ba2e6
Kevin <whiteowl0001@gmail.com>
2017-07-17 04:50:17 +0000

    Created Eloquent Video

--- resources/views/admin/video.blade.php
+++ resources/views/admin/video.blade.php
@@ -17,7 +17,7 @@
                              <div class="panel panel-default">
                                 <div class="panel-heading">Videos</div>
                                     <div class="panel-body">
-                            @foreach ($Videos as $video)
+                         <!--   @foreach ($Videos as $video)
                                 <div class="container">
                                     <div class="row">
                                         <div class="col-sm-4 video-layout">
@@ -27,7 +27,7 @@
                                         </div>
                                     </div>
                                 </div>
-                            @endforeach
+                            @endforeach -->
                         </div>
                     </div>
                      </div>
