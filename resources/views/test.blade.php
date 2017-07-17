f48b518bf417659c6a917fea31c02af512bfa8dc
Kevin <whiteowl0001@gmail.com>
2017-07-17 07:22:40 +0000

    Created Eloquent Video

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,25 +4,21 @@
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+      @foreach( $Images as $Image )
+         <li data-target="#carousel-example-generic" data-slide-to="{{ $loop->index }}" class="{{ $loop->first ? 'active' : '' }}"></li>
+      @endforeach
   </ol>
 
   <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
         @foreach ($Images as $Image)
-            <div class="item active">
+            <div class="item {{ $loop->first ? ' active' : '' }}">
               <img class="img-responsive center-block" src="/carousel/{{$Image->original_filename}}" alt="...">
               <div class="carousel-caption">
                 ...
               </div>
             </div> 
         @endforeach
-        <div class="item active">
     </div>
 
   <!-- Controls -->
