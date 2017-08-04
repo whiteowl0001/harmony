dd660bd9aab1a5c1ed1438ba99b8b724364746b0
Kevin <whiteowl0001@gmail.com>
2017-08-04 16:05:43 +0000

    Updated Admin Section

--- resources/views/admin/projects.blade.php
+++ resources/views/admin/projects.blade.php
@@ -6,7 +6,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                           Projects
+                           Carousel
                         </h1>
                         <ol class="breadcrumb">
                             <li class="active">
--- resources/views/admin/video.blade.php
+++ resources/views/admin/video.blade.php
@@ -32,7 +32,7 @@
             		    </div>
                         <div class="container">
                             <div class="row">
-                                <div class="col-md-8">
+                                <div class="col-md-12">
                                     <div class="panel panel-default">
                                         <div class="panel-heading">Projects</div>
                                             <div class="panel-body">
