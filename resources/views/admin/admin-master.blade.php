e32bbdb7c06ca5e60d86e91b4cc405bdb8329c2e
Kevin <whiteowl0001@gmail.com>
2017-08-04 16:14:07 +0000

    Updated Admin Section

--- resources/views/admin/admin-master.blade.php
+++ resources/views/admin/admin-master.blade.php
@@ -46,18 +46,11 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">SB Admin</a>
+                <a class="navbar-brand">SB Admin</a>
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>Joe <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="/logout"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
-                        </li>
-                    </ul>
-                </li>
+                <a href="/logout"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
             </ul>
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
             <div class="collapse navbar-collapse navbar-ex1-collapse">
