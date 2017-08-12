5cf3d9f8d2bd3d9b87cb098d29d2b11b76a8c2e2
Kevin <whiteowl0001@gmail.com>
2017-08-12 19:39:38 +0000

    Added Review link

--- resources/views/admin/reviews.blade.php
+++ resources/views/admin/reviews.blade.php
@@ -18,7 +18,7 @@
                             <div class="panel-body">
                                 <form enctype="multipart/form-data" action="upload/review" method="POST">
                                 {!! csrf_field() !!}
-                                    <div class="col-lg-2">
+                                    <div class="col-lg-5">
                                                                 <!--Add Video-->
                                         <p>Reviews</p>
                                     </div>
