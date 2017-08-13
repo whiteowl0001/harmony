8e2a2cd17097a854a364f802c74d656e423fce60
Kevin <whiteowl0001@gmail.com>
2017-08-13 22:16:23 +0000

    Reviews Page Update

--- resources/views/testimonial.blade.php
+++ resources/views/testimonial.blade.php
@@ -1,6 +1,240 @@
 @extends('layout.master')
 @section('content')
-
+<main role="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="row">
+					<div class="col-sm-9">
+						<div class="content">
+							<h1>Reviews</h1>
+								<div class="review">
+									<h2>Refinish Already Installed Cabinets</h2>
+									<div class="review__rating">
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+                                    </div>
+									<p class="review__content">Great customer service. 
+									I have already recommended them to family and friends.</p>
+									<div class="review__footer">
+										-Ron F. | June 2017
+									</div>
+								</div>
+								<div class="review">
+									<h2>Paint, Varnish or Stain Interior Surfaces.</h2>
+									<div class="review__rating">
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+                                    </div>
+									<p class="review__content">Arrived on time, got right to work.
+									Did an exceptional job of painting my 50+ year old condo. 
+									The price was great and my place looks like new. Even the 
+									cleanup was professional. Could never have gotten this job 
+									done in 1 1/2 days without Harmony Painting. Highly recommended. 
+									Look no further; hire Harmony Painting.<br></p>
+									<div class="review__footer">
+										-Lorraine C. | May 2017
+									</div>
+								</div>
+								<div class="review">
+									<h2>Paint, Varnish or Stain Interior Surfaces</h2>
+									<div class="review__rating">
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+                                    </div>
+									<p class="review__content">Joe did a fantastic job
+									repairing my plaster and lathe walls, helping me 
+									with color choices, was professional, timely, responsive,
+									cleaned up each day better than I could, had reasonable
+									rates and was meticulous in his work product resulting
+									in a beautiful room. He also offered and removed a dresser 
+									that I could not manage. I would highly recommend his company.</p>
+									<div class="review__footer">
+										-Mary K. | July 2017
+									</div>
+								</div>
+							
+								<div class="review">
+									<h2>Paint or Stain Interior Surfaces</h2>
+									<div class="review__rating">
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+                                    </div>
+									<p class="review__content">He did extra work for me, 
+									and didn't even charge me. On schedule, good price 
+									and totally cleans up after the work is done.</p>
+									<div class="review__footer">
+										-Joe/Toni A. | April 2017
+									</div>
+								</div>
+							
+								<div class="review">
+									<h2>Paint or Stain Interior Surfaces</h2>
+									<div class="review__rating">
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+                                    </div>
+									<p class="review__content">Harmony Painting was very un-intrusive in their process. They were flexible with my schedule. It was like they were never there. Joseph is reasonably priced. He was always on time, very professional, and did great work.</p>
+									<div class="review__footer">
+										-Christopher G.  | April 2017
+									</div>
+								</div>
+							
+								<div class="review">
+									<h2>Paint, Varnish or Stain Interior Surfaces</h2>
+									<div class="review__rating">
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+                                    </div>
+									<p class="review__content">Not only did he do a very good job, he was very honest and very punctual. If he was going to be 5 minutes late he texted and let me know. The quality of the work was excellent. It was a very good experience. He is very responsible and I would use him again.</p>
+									 <div class="review__footer">
+									     -Ruth M. | March 2017
+									</div>
+								</div>
+							
+								<div class="review">
+									<h2>Paint or Stain Interior Surfaces</h2>
+									<div class="review__rating">
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+                                    </div>
+									<p class="review__content">He is a nice young guy and did a great job.
+									</p>
+									<div class="review__footer">
+										-Joseph J. | March 2017
+									</div>
+								</div>
+							
+								<div class="review">
+									<h2> Paint or Stain Interior Surfaces</h2>
+									<div class="review__rating">
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+                                    </div>
+									<p class="review__content">
+                                        Very accommodating and couldn't have asked for a better guy. They were on the lower end of the spectrum from the other quotes I got, They were very professional, clean, don't make a mess, did what they said they would, and were punctual.
+									</p>
+									<div class="review__footer">
+										-Ken W. | March 2017
+									</div>
+								</div>
+							
+								<div class="review">
+									<h2>Refinish Already Installed Cabinets</h2>
+									<div class="review__rating">
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+                                    </div>
+									<p class="review__content">
+                                        A very good experience --- Joe from Harmony Painting made the job run very smoothly without taking away from the day to day operations in a home. The work area was very neat, very considerate, full explanation before and during the work -- no surprises --- job was exactly what I was hoping it would be.									<div class="review__footer">
+									</p>
+										- | Febuary 2017
+									</div>
+								</div>
+							
+								<div class="review">
+									<h2>Paint or Stain Interior Surfaces</h2>
+									<div class="review__rating">
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+										<i class="fa fa-star" aria-hidden="true"></i>
+                                    </div>
+									<p class="review__content">
+                                        They were clean and professional for both jobs.	
+                                    </p>
+                                    <div class="review__footer">
+										-Jay P. | Febuary 2017
+									</div>
+								</div>
+						</div>
+					</div>
+		<div class="col-sm-3 hidden-xs">
+            <div class="nav-aside-container">
+				<div class="nav-aside-header">Write a Review</div>
+					<div class="nav-aside">
+						<div class="review-form">
+                            <form id="ReviewForm_ReviewForm" action="/reviews/ReviewForm" method="post" enctype="application/x-www-form-urlencoded">
+                            	<p id="ReviewForm_ReviewForm_error" class="message " style="display: none"></p>
+                            	<fieldset>
+                            		<div id="Rating" class="field dropdown">
+                            	    <label class="left" for="ReviewForm_ReviewForm_Rating">Rating</label>
+                            	        <div class="middleColumn">
+                            		        <select name="Rating" class="dropdown" id="ReviewForm_ReviewForm_Rating" required="required" aria-required="true">
+                                            	<option value="5">5 Stars</option>
+                                            	<option value="4">4 Stars</option>
+                                            	<option value="3">3 Stars</option>
+                                            	<option value="2">2 Stars</option>
+                                            	<option value="1">1 Star</option>
+                                            </select>
+                                    	</div>
+                                    </div>
+			                        <div id="Name" class="field text">
+                                    	<label class="left" for="ReviewForm_ReviewForm_Name">Name</label>
+                                    	<div class="middleColumn">
+                                    		<input type="text" name="Name" class="text" id="ReviewForm_ReviewForm_Name" required="required" aria-required="true">
+                                    	</div>
+                                    </div>
+			                        <div id="Email" class="field email text">
+                                    	<label class="left" for="ReviewForm_ReviewForm_Email">Review Title</label>
+                                    	<div class="middleColumn">
+                                    		<input type="email" name="Email" class="email text" id="ReviewForm_ReviewForm_Email" required="required" aria-required="true">
+                                    	</div>
+                                    </div>
+			                        <div id="Content" class="field textarea">
+                                    	<label class="left" for="ReviewForm_ReviewForm_Content">Your review</label>
+                                    	<div class="middleColumn">
+                                    		<textarea name="Content" class="textarea" id="ReviewForm_ReviewForm_Content" required="required" aria-required="true" rows="5" cols="20"></textarea>
+                                    	</div>
+                                    </div>
+		                            	<input type="hidden" name="SecurityID" value="3a80941fd7d0552aa693e5d52a48905f9546f67d" class="hidden" id="ReviewForm_ReviewForm_SecurityID">
+	                            	<div class="clear"><!-- --></div>
+	                            </fieldset>
+                                <div class="form-group">
+	                                <div class="Actions">
+                                    	<input type="submit" name="action_Submit" value="Submit" class="action" id="ReviewForm_ReviewForm_action_Submit">
+                                    </div>
+                        	    </div>
+                            </form>
+			                    	<p class="review-form__disclaimer">* In submitting a review of the services provided to you, you are authorizing a posting of the review on this website. Unless you indicate otherwise, we will not share your information or identity beyond your initials.</p>
+                                </div>
+                            </div>
+                        </div>
+					</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<!--
 <div id="rating">
     <h1>What Our Customers Have To Say!</h1>
     <div class="container">
@@ -355,4 +589,5 @@
         </div>
     </div>
 </div>
+-->
 @stop
