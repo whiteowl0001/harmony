5d74a4958bf70cb6066d36cce9f9f31e49d86cc2
Kevin <whiteowl0001@gmail.com>
2017-08-18 21:27:33 +0000

    Added Rewviews Form

--- resources/views/admin/reviews.blade.php
+++ resources/views/admin/reviews.blade.php
@@ -13,46 +13,28 @@
                                 <i class="fa fa-dashboard"></i> Dashboard
                             </li>
                         </ol>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">Add Review</div>
-                            <div class="panel-body">
-                                <form enctype="multipart/form-data" action="upload/review" method="POST">
-                                {!! csrf_field() !!}
-                                    <div class="col-lg-12">
-                                                                <!--Add Video-->
-                                        <p>Reviews</p>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="select" class="col-lg-12 control-label">Rating</label>
-                                        <div class="col-lg-12">
-                                            <select class="form-control" id="stars" name="stars">
-                                                <option>1</option>
-                                                <option>2</option>
-                                                <option>3</option>
-                                                <option>4</option>
-                                                <option>5</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                     <div class="form-group">
-                                        <label for="select" class="col-lg-12 control-label">Name</label>
-                                        <input type="text" class="form-control col-lg-12" id="reviewed_by" name="reviewed_by">
-                                        <br>
-                                    </div>
-                                     <div class="form-group">
-                                        <label for="select" class="col-lg-12 control-label">Review Title</label>
-                                        <input type="text" class="form-control" id="project_title" name="project_title">
-                                        <br>
-                                    </div>
-                                     <div class="form-group col-lg-10">
-                                        <label for="select" class="col-lg-12 control-label">Your Review</label>
-                                        <textarea type="textarea" name="project_description" placeholder="Project Description"></textarea>
-                                        <br>
-                                    </div>
-                                    <input type="submit" class="pull-right btn btn-sm btn-primary">
-                                </form>
-            		        </div>
-            		    </div>
+                        <div class="col-lg-6">
+                            <h2>Reviews</h2>
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Images</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($Reviews as $Review)
+                                        <tr>
+                                            <td>{{$Review->reviewed_by}}</td>
+                                            <td>{{$Review->project_title}}</td>
+                                            <td>{{$Review->project_description}}</td>
+                                            <td><button type="button" class="btn btn-lg btn-danger">Delete</button></td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
