e5894310b00bfd2c85906a23f547064a9fa4b946
Kevin <whiteowl0001@gmail.com>
2017-08-27 22:15:27 +0000

    Added Google Analytics

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -18,12 +18,16 @@
 	<link href="css/reviews/custom.css" rel="stylesheet">
 	<script src="https://use.fontawesome.com/ad5057c19b.js"></script>
 	
-    <!-- =======================================================
-        Theme Name: Gp
-        Theme URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-templat/
-        Author: BootstrapMade
-        Author URL: https://bootstrapmade.com
-    ======================================================= -->  
+    		<!--=====Google Analytics.=====-->  
+    <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-105335120-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
     
   </head>
    <body class="homepage"> 
